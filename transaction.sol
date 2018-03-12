b7928c3cb9a26892cc23878c7fd9184900f53221
Anugrahaa <vr.anugrahaa@gmail.com>
2018-03-13 02:09:26 +0530

    updated smart contract

--- transaction.sol
+++ transaction.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18
 
 
 Contract Transaction{
-	struct business{
+	struct Business{
 		bytes16 name;
 		uint category;
 	}
@@ -11,7 +11,7 @@ Contract Transaction{
 	mapping(bytes16 => business) businesses;
 	bytes16[] public GSTIN;
 
-	struct product{
+	struct Product{
 		bytes16 hsn;
 		bytes16 name;
 		uint rate;
@@ -19,17 +19,64 @@ Contract Transaction{
 	mapping(bytes16 => product) products;
 	bytes16[] public productid;
 
-	struct transaction{
-		business from;
-		business to; 
-		product p;
+	struct Transaction{
+		bytes16 from;
+		bytes16 to; 
+		bytes16 p;
 		uint quantity;
 	}
 	mapping(uint => transaction) transactions;
-	uint[] public transid;
+	uint nexttransid;
 
 	function setBusiness(bytes16 _GSTIN, bytes16 _name, uint _category){
-		
+		var business = businesses[_GSTIN];
+
+		business.name = _name;
+		business.category = _category;
+
+		GSTIN.push(_GSTIN) -1;
+	}
+
+	function setProduct(bytes16 _productid, bytes16 _hsn, bytes16 _name, uint _rate){
+		var product = products[_productid];
+		product.hsn = _hsn;
+		product.name = _name;
+		product.rate = _rate;
+
+		productid.push(_productid) -1;
+	}	
+
+	function setTransaction(bytes16 from_GSTIN, bytes16 to_GSTIN, bytes16 _productid, uint _quantity) returns (uint id){
+		var transaction = transactions[nexttransid];
+		transaction.from = from_GSTIN;
+		transaction.to = to_GSTIN;
+		transaction.p = _productid;
+		transaction.quantity = _quantity;
+
+		id = nexttransid;
+		nexttransid++;
+
+		return id;
+	}
+
+	function getBusinesses() public view returns (bytes16){
+		return GSTIN;
+	}
+
+	function getProducts() public view returns (bytes16) {
+		return productid;
+	}
+
+	function getBusiness(bytes16 gstin) public view returns(bytes16, uint){
+		return (businesses[gstin].name, businesses[gstin].category);
+	}
+
+	function getProduct(bytes16 prodid) public view returns(bytes16, bytes16, uint){
+		return (products[prodid].hsn, products[prodid].name, products[prodid].rate);
+	}
+
+	function getTransaction(uint transactionid) public view returns (bytes16, bytes16, bytes16, uint){
+		return (transactions[transactionid].from, transactions[transactionid].to, transactions[transactionid].p, transactions[transactionid].quantity);
 	}
 
 }
